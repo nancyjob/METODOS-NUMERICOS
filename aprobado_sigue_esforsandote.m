5e4ca73be4c437ba2fab5eb76dceb168f50d4f4a
nancyjob <43286613+nancyjob@users.noreply.github.com>
2018-10-03 15:54:37 -0500

    Add files via upload

---
+++ aprobado_sigue_esforsandote.m
@@ -0,0 +1,8 @@
+'C7alificación es aprobatoria o reprobatoria'
+prompt=('Introduce la calificación: ')
+calif=input(prompt);
+    if calif>=7
+        fprintf('Felicidades has aprobado')
+    else
+        fprintf('Continua con tus estudios, necesitas esforzarte mas')
+    end
---
+++ cuadrado_cubo.m
@@ -0,0 +1,8 @@
+'Elevar al cuadrado un numero si es par y elevar al cubo un numero impar'
+prompt='Introduce un numero: '
+ c=input(prompt);
+   if  rem(c,2)==0
+    c^2    
+   else
+    c^3
+ end
