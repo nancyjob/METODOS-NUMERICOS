672d3e655e83d599f447c033424cb2616631c8c7
nancyjob <43286613+nancyjob@users.noreply.github.com>
2018-10-03 19:34:15 -0500

    Add files via upload

---
+++ sistema_de_cobro.m
@@ -0,0 +1,16 @@
+prompt='Ingrese litros de agua que consumio al mes '
+L=input(prompt);
+    if  (L>49)
+        if (L<201)
+        fprintf('el costo que pagara por el consumo es:')
+        CARGO1=(L*3)
+        CARGOFIN1=(CARGO1-50)
+        end
+    end
+    if(L>200)
+        fprintf('El costo que pagara por el consumo es')
+        CARGO2=(L*5)
+        CARGOFIN2=(CARGO2-50)
+    else
+        fprintf('El costo que pagara por el consumo es de 150')
+    end
